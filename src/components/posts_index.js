aef56a01a0ea79f1615bd3027586f78cc8922957
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-29 12:58:25 -0400

    install axios and redux-promise, wire in the middleware, and make api request in actions to get a list of posts

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+	render(){
+		return(
+			<div>
+
+				list of posts 
+			</div>)
+	}
+}
+
+export default PostsIndex;
