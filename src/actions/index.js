16b5ecd9d2b248db71b26ef16271a05e44396035
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-29 17:57:30 -0400

    finish posts list component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = '?key=yang123'
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=jessieyang123123123';
 
 export function fetchPosts(){
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
